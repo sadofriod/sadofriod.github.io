e1c6a0e263e7958ecc59358e9f30ed719989c276
ashesborn <792634614@qq.com>
2025-06-08 15:15:46 +0800

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,6 @@
 import { getBlogPosts } from "./blog/utils"
 
-export const baseUrl = process.env.NEXT_PUBLIC_SITE_URL
+export const baseUrl = process.env.NEXT_PUBLIC_SITE_URL ?? 'https://blog.ashesborn.cloud' 
 
 export default async function sitemap() {
   const blogs = getBlogPosts().map((post) => ({
